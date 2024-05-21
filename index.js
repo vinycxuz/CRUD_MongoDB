a85366a1031880e1999974d9b9d5051f4eb56a6b
vinycxuz <vinyby@gmail.com>
2024-05-21 12:00:31 -0300

    feat(index) install mongoose, mongodb and connect requiring mongo module and connect the string cluster

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const { configDotenv } = require('dotenv');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
@@ -7,4 +11,12 @@ app.listen(3000, () => {
 
 app.get('/', (req, res) => {
   res.send('test');
+});
+
+mongoose.connect(`mongodb+srv://crudmongo:${process.env.PASSWORDMONGO}@crudmongo.4v8cpdw.mongodb.net/?retryWrites=true&w=majority&appName=crudmongo`)
+.then(() => {
+  console.log('Connected to MongoDB');
+})  
+.catch((err) => {
+  console.log('Failed to connect to MongoDB', err);
 });
