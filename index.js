396d142154b7c9e94f431bba1b06b4c71ef9d369
vinycxuz <vinyby@gmail.com>
2024-05-22 19:19:12 -0300

    feat(index): created a put method using id for request

--- index.js
+++ index.js
@@ -51,6 +51,23 @@ app.get('/love/:id', async (req, res) => {
   }
 });
 
+app.put ('/love/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const love = await Love.findByIdAndUpdate(id, req.body);
+    
+    if(!love) {
+      return res.status(404).send('Love not found');
+    }
+
+    const updatedLove = await Love.findById(id);
+    res.status(200).json(updatedLove);
+
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 
 mongoose.connect(process.env.STRING_CONNECTION)
 .then(() => {
